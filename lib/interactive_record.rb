5066a0011d23fb5545f4ddcd1afc4d580171acdc
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-07-19 15:29:14 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -50,7 +50,7 @@ class InteractiveRecord
   end
 
   def self.find_by_name(name)
-    binding.pry
+    # binding.pry
     sql = "SELECT * FROM #{self.table_name} WHERE name = #{name}"
     DB[:conn].execute(sql)
   end
