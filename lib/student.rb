dfa71acbd1b306a805eaa905efde655359c6299e
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-07-19 17:43:30 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -3,5 +3,14 @@ require 'active_support/inflector'
 require 'interactive_record.rb'
 
 class Student < InteractiveRecord
+  self.column_names.each do |col|
+    attr_accessor col.to_sym
+  end
+
+  def self.find_by_name(name)
+    sql = "SELECT * FROM #{self.table_name} WHERE name = ?"
+    DB[:conn].execute(sql,name)
+  end
+
 
 end
