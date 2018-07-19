341a9b47f3edde459e409390e15629683096ddfc
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-07-19 14:57:46 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -7,4 +7,23 @@ class InteractiveRecord
     self.to_s.downcase.pluralize
   end
 
+  def self.column_names
+    DB[:conn].results_as_hash = true
+    sql = <<-SQL
+    PRAGMA table_info('#{table_name}')
+    SQL
+    table_info = DB[:conn].execute(sql)
+    columns = []
+    table_info.each {|col| columns << col['name']}
+      
+
+
+
+
+
+
+
+
+
+    
 end
